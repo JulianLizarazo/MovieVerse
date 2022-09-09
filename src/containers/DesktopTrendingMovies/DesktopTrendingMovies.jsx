4255ecadd68a6574af176427952ea6bfd04d65ab
Julian Lizarazo <julianca20122001@hotmail.com>
2022-09-08 23:13:45 -0500

    slider almost finished

--- src/containers/DesktopTrendingMovies/DesktopTrendingMovies.jsx
+++ src/containers/DesktopTrendingMovies/DesktopTrendingMovies.jsx
@@ -1,21 +1,41 @@
 import { useTrendingMovies } from "../../hooks/useTrendingMovies";
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  EffectFade,
+} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
 import "./DesktopTrendingMovies.scss";
 import { DesktopTrendingMovie } from "../../components/DesktopTrendingMovie/DesktopTrendingMovie";
+import { AiFillRightCircle, AiFillLeftCircle } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+const animateProps = {
+  scale: 1.5,
+  textShadow: "0px 0px 8px rgb(255,255,255)",
+};
 
 const DesktopTrendingMovies = () => {
   const { trendingMovies } = useTrendingMovies();
+  const swiperRef = useRef(null);
 
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
       slidesPerView={1}
-      autoplay={{ delay: 5000 }}
-      pagination={{ clickable: true }}
-      onSlideChange={() => console.log("slide change")}
+      effect={"fade"}
+      speed={1000}
+      autoplay={{ delay: 5000, disableOnInteraction: false }}
       loop={true}
+      className="swiper"
+      ref={swiperRef}
     >
       {trendingMovies.map((trendingMovie) => (
         <SwiperSlide className="trending-movies-img">
@@ -38,6 +58,20 @@ const DesktopTrendingMovies = () => {
           </div>
         </SwiperSlide>
       ))}
+      <motion.div
+        whileHover={animateProps}
+        id="next-button"
+        onClick={() => swiperRef.current.swiper.slideNext()}
+      >
+        <AiFillRightCircle className="slider-icon" />
+      </motion.div>
+      <motion.div
+        whileHover={animateProps}
+        id="previous-button"
+        onClick={() => swiperRef.current.swiper.slidePrev()}
+      >
+        <AiFillLeftCircle className="slider-icon" />
+      </motion.div>
     </Swiper>
   );
 };
