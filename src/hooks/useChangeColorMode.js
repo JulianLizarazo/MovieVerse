33abe4ce285bbd5cc94b8f515d14a1410fef7143
JulianLizarazo <julianca20122001@hotmail.com>
2022-12-21 15:51:12 -0500

    fixed color theme on first loading

--- src/hooks/useChangeColorMode.js
+++ src/hooks/useChangeColorMode.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
 
-const initialState = {
-  colorMode: "dark",
-};
+const initialState = "light";
 
 export const useChangeColorMode = () => {
   const [theme, setTheme] = useState(() => {
     const localData = localStorage.getItem("colorMode");
-
     return localData ? JSON.parse(localData) : initialState;
   });
   const changeThemeMode = () => {
